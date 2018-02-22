632a80745846be347b49736bda8dcc2ce009786f
arnaudcosson <arnaud.cosson@decathlon.com>
2018-02-22 14:53:00 +0100

    chore(env): use Rails.env in config loading

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module RubyApi
 
     if Rails.env.development?
       config.before_configuration do
-        env_file = File.join(Rails.root, 'config', 'development_env.yml')
+        env_file = File.join(Rails.root, 'config', Rails.env+'_env.yml')
         if File.exists?(env_file)
           YAML.load(File.open(env_file)).each do |key, value|
             ENV[key.to_s] = value
